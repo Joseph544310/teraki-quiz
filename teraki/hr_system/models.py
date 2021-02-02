50805c3ee36f44cc3a61f1d3cc2118d6be02d762
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-02 19:56:46 +0100

    Added Employee model and registered it in admin

--- teraki/hr_system/models.py
+++ teraki/hr_system/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=400)
+    role = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    salary = models.PositiveIntegerField()
