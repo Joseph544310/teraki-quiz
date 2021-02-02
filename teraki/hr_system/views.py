c16df543b68f709f5e746c48b6daf71cc83c1aa9
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-02 20:51:42 +0100

    Added class based views for CRUD using drf.generics

--- teraki/hr_system/views.py
+++ teraki/hr_system/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from .serializers import EmployeeSerializer
+from .models import Employee
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 # Create your views here.
+
+
+class ListOrCreate(ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class RetrieveOrUpdateOrDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
