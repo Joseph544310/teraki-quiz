17dadbdba1d4091280e26cf79125e5cd4d0cd9d6
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-03 16:04:48 +0100

    Added sortBy functionality in EmployeesTable

--- client/src/components/EmployeesTable.tsx
+++ client/src/components/EmployeesTable.tsx
@@ -14,6 +14,7 @@ interface IEmployee {
 const EmployeesTable: React.FC = props => {
     const [employees, setEmployees] = useState<IEmployee[]>([])
     const [searchKey, setSearchKey] = useState('')
+    const [sortingOption, setSortingOption] = useState<keyof IEmployee>('id')
 
     useEffect(() => {
         // Fetch all employees
@@ -28,31 +29,52 @@ const EmployeesTable: React.FC = props => {
 
     return (
         <div>
-            <input 
-            type='text' 
-            value={searchKey} 
-            onChange={e => setSearchKey(e.target.value)}
-            placeholder='Search by name...'/>
+            {employees.length>0?
+            <div className='controls'>
+                <input 
+                type='text' 
+                value={searchKey} 
+                onChange={e => setSearchKey(e.target.value)}
+                placeholder='Search by name...'/>
 
+                <label htmlFor='sort-by'>Sort By</label>
+                <select
+                onChange={e => {
+                    if (e.target.value === 'id' || e.target.value === 'name' || e.target.value === 'address' ||
+                     e.target.value === 'role' || e.target.value === 'department' || e.target.value === 'salary' )
+                    {
+                            setSortingOption(e.target.value)
+                    }
+                }}
+                id='sort-by'>
+                    {Object.keys(employees[0]).map(key =>
+                    <option key={key} value={key}>{key}</option>)}
+                </select>
+            </div>: null}
+
+            {employees.length>0?  
             <Table>
                 <thead>
-                        <tr>
-                            {Object.keys(employees[0]).map(key =>
-                            <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>)}
-                        </tr>
+                    <tr>
+                        {Object.keys(employees[0]).map(key =>
+                        <th key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}</th>)}
+                    </tr>
                 </thead>
                 <tbody>
-                    {employees?.filter(employee => employee.name.includes(searchKey)).map(employee => {
-                        return (
-                            <tr key={employee.id}>
-                                {Object.entries(employee).map(entry => 
-                                    <td key={entry[0]}>{entry[1]}</td>)}
-                            </tr>
-                        )
-                    }
+                    {employees?.filter(employee => employee.name.includes(searchKey))
+                    .sort((a, b) => {
+                        if (a[sortingOption] < b[sortingOption]) return -1
+                        else return 1
+                    })
+                    .map(employee => 
+                        <tr key={employee.id}>
+                            {Object.entries(employee).map(entry => 
+                                <td key={entry[0]}>{entry[1]}</td>)}
+                        </tr>
                     )}
                 </tbody>
-            </Table>
+            </Table>:
+            <p>No Data Yet!!</p>}
         </div>
     )
 }
